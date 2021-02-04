10d050ed736c0868037ce87643cd24f15061aa16
Divyabd <divyagowda8393@gmail.com>
2021-02-04 22:12:06 +0530

    second commit

---
+++ One/src/one/One.java
@@ -0,0 +1,13 @@
+package one;
+
+public class One {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Divya");
+		System.out.println("kousalya");
+	}
+
+}
